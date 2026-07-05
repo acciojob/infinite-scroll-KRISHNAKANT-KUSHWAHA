beb3c73a7cb41b2b65d4b52e69c86763cdcb0c74
Krishnakant Kushwaha <145730098+KRISHNAKANT-KUSHWAHA@users.noreply.github.com>
2026-07-05 18:53:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
+// const list=document.getElementById("infi-list");
+// const item=document.createElement("li");
+// item.innerHTML="Item 11";
+// list.appendChild(item);
 
+
+const list = document.getElementById("infi-list");
+let itemCount = list.children.length;
+
+list.addEventListener("scroll", () => {
+
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+
+        for (let i = 0; i < 2; i++) {
+            itemCount++;
+
+            const li = document.createElement("li");
+            li.textContent = "Item " + itemCount;
+
+            list.appendChild(li);
+        }
+
+    }
+
+});
